108473cfa69002911fe6770d3bc996ff72b54ff0
rakhi-rara <rokayaarakhi@gmail.com>
2025-04-29 00:19:28 +0200

    Added types, helpers, and session management

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,22 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import type { User } from '$lib/types';
+
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: import('$lib/types').SafeUser | null;
+		}
+		// You can extend other types below if needed in the future
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
 }
 
+declare module 'set-cookie-parser';
+
 export {};
+
+
